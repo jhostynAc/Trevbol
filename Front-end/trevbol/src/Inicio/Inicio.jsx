9db11583c8d96f32b2d6ded6bd5adab51fa58830
JhostynAc <jhostyn802@gmail.com>
2026-02-23 12:56:55 -0500

    inicio completo sin botones funcionales

--- Front-end/trevbol/src/Inicio/Inicio.jsx
+++ Front-end/trevbol/src/Inicio/Inicio.jsx
@@ -1,13 +1,13 @@
 import './inicio.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import camisafon from '../Img/machape.png';
 import logo from '../Img/Logo.png';
 function inicio() {
     return (
         <body>
-            <div className="contenedor_principal">
-                <nav className="navbar">
+             <nav>
                     <div className="logo"><img src={logo} alt="logo" /></div>
                     <div className="botones">
                         <ul>
@@ -19,6 +19,7 @@ function inicio() {
                         </ul>
                     </div>
                 </nav>
+            <div className="contenedor_principal">
                 <div className="container1">
                     <div className="container">
                         <h2>Trevbol</h2>
@@ -32,8 +33,37 @@ function inicio() {
                     </div>
                 </div>
             </div>
-        </body>
+            <div className="titulo">
+                <h3>Collage de productos</h3>
+            </div>
+            <div className="container_collage">
+                <div class="parent">
+                    <div class="div1"></div>
+                    <div class="div2"></div>
+                    <div class="div3"></div>
+                    <div class="div4"></div>
+                    <div class="div5"></div>
+                </div>
+            </div>
+            <footer>
+                <div className="contacto">
+                    <p>Contactanos:</p>
+            </div>
+                <div className="redes">
+                    <FontAwesomeIcon icon={faInstagram} />
+                    <FontAwesomeIcon icon={faFacebook} />
 
+                </div>
+                    <p>© 2026 Trevbol. Todos los derechos reservados.</p>
+            </footer>
+        </body>
+      
     );
 }
+
+window.addEventListener('scroll', function () {
+    var nav = this.document.querySelector('nav');
+    nav.classList.toggle('bajar_1', window.scrollY > 0);
+});
+
 export default inicio;
